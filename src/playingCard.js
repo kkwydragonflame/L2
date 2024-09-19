ce72c87997eafce4a223b66fb2df91737412b84e
Johanna Eriksson <je224gs@student.lnu.se>
2024-09-19 14:21:39 +0200

    Set mutator methods as private to prevent manipulation after object creation

--- src/playingCard.js
+++ src/playingCard.js
@@ -3,11 +3,11 @@ export class PlayingCard {
   #rank
 
   constructor(suit, rank) {
-    this.suit = suit;
-    this.rank = rank;
+    this.#setSuit(suit)
+    this.#setRank(rank)
   }
 
-  set suit(suit) {
+  #setSuit(suit) {
     //valid suits &#9824; &#9827; &#9829; &#9830 for black icons
     // valid suits for white icons: ♠ ♣ ♥ ♦ &#9828; &#9825; &#9831; &#9826;
     // ['♠', '♣', '♥', '♦', '♤', '♧', '♡', '♢']
@@ -23,7 +23,7 @@ export class PlayingCard {
     return this.#suit
   }
 
-  set rank(rank) {
+  #setRank(rank) {
     const validRanks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     if (validRanks.includes(rank)) {
       this.#rank = rank
@@ -37,16 +37,16 @@ export class PlayingCard {
   }
 
   valueOf() {
-    if (this.rank === 'J') {
+    if (this.#rank === 'J') {
       return 11
-    } else if (this.rank === 'Q') {
+    } else if (this.#rank === 'Q') {
       return 12
-    } else if (this.rank === 'K') {
+    } else if (this.#rank === 'K') {
       return 13
-    } else if (this.rank === 'A') {
-      return 14
+    } else if (this.#rank === 'A') {
+      return 14 //What i ace needs to be counted as one?! Depends on what game you are playing
     } else {
-      return parseInt(this.rank)
+      return parseInt(this.#rank)
     }
   }
 }
