2fe90dd83a496e44237e5ff389f27f8ee69c771f
Johanna Eriksson <je224gs@student.lnu.se>
2024-09-30 22:49:03 +0200

    Update jest config to include coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,26 @@
 export default {
-  testEnvironment: 'node'
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+
+    coverageReporters: [
+     "json",
+     "text",
+     "lcov",
+     "clover"
+  ],
+    coverageDirectory: "coverage",
+    
+      collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",  // Target files in the src folder
+    "!src/**/index.js",         // Optionally exclude specific files (e.g., index.js)
+    "!**/node_modules/**",       // Exclude node_modules
+
+    "!**/vendor/**",             // Exclude vendor files
+  ],
+
+   collectCoverage: true,
+
+ clearMocks: true,
 }
